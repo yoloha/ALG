c15dcc27a785bb44c54fae430fd31c3186b11dfc
WeiningChen <wnchen1994@gmail.com>
2015-05-18 19:04:58 +0800

    adding directory

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
  	BoundingBox Bbox;
- 	ifstream ifs("./case_ex");
+ 	ifstream ifs("../inputs/case_ex");
  	Bbox.readBlock(ifs);
  	Bbox.buildGroup();
 	Bbox.buildWindow();
